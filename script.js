026fd4bd53bc2e865d65b41ff7f0db4c06dba5b9
vivekranjan4256 <79060614+vivekranjan4256@users.noreply.github.com>
2021-03-14 10:24:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
+
+for(var i=0; i< names.length; i++){
+	if(names[i].charAt(0)=='J'|| names[i].charAt(0)=='j'){
+		console.log("Good Bye "+names[i])
+	}
+	else{
+		console.log("Hello "+names[i])
+	}
+}
